b276cc9a011d2914e2ff0c95277d3635828e9d64
Thalles Eduardo Oliveira Bueno <thalles_bueno@terceiros.sicredi.com.br>
2023-01-04 15:20:20 -0300

    add logger middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
 import Logger from './config/logger';
+import LoggerMiddleware from './config/loggerMiddleware';
 
 const app = express();
+
+app.use(LoggerMiddleware);
 
 app.get('/', (req, res) => {
   res.send('hello world');
