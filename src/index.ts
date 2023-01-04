8060edb44c15511380d09d76a6e0788ee2587cb2
Thalles Eduardo Oliveira Bueno <thalles_bueno@terceiros.sicredi.com.br>
2023-01-04 15:22:48 -0300

    add cors config to express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 import Logger from './config/logger';
 import LoggerMiddleware from './config/loggerMiddleware';
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
 app.use(LoggerMiddleware);
 
 app.get('/', (req, res) => {
