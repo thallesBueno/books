8cac8589081880ee27edf6e053bc13509b149723
Thalles Eduardo Oliveira Bueno <thalles_bueno@terceiros.sicredi.com.br>
2023-01-04 15:12:30 -0300

    install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('oi');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+const defaultPort = 3000;
+
+app.listen(defaultPort, () => {
+  console.log(`App listening at port ${defaultPort}.`);
+});
