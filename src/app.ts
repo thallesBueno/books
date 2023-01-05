1eee6ae3c6d1ce65cac6d7af651c4fa1f4ec1bbb
Thalles Eduardo Oliveira Bueno <thalles_bueno@terceiros.sicredi.com.br>
2023-01-05 16:04:59 -0300

    add basic routing

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,15 @@ import cors from 'cors';
 import Logger from './config/logger';
 import LoggerMiddleware from './config/loggerMiddleware';
 
+import Routes from './controllers/routes';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(LoggerMiddleware);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+app.use(Routes);
 
 const startApp = (port: number) => {
   app.listen(port, () => {
